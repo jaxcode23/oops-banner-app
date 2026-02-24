4170f233456675ba8bd626d978f07715f2748ff7
jaxcode23 <jashajmera008@gmail.com>
2026-02-24 08:54:23 +0530

    UC4: Refactor OOPS banner using String array and enhanced for-loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -11,4 +11,52 @@ public class OOPSBannerApp {
         System.out.println(" *****    *****    *         ****** ");
 
     }
+}public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // Store all banner lines inside a String array
+        String[] banner = {
+
+                String.join("", " ***** ", "   ",
+                        " ***** ", "   ",
+                        " ****** ", "   ",
+                        " ****** "),
+
+                String.join("", "*     *", "  ",
+                        "*     *", "   ",
+                        "*     *", "   ",
+                        "*      "),
+
+                String.join("", "*     *", "  ",
+                        "*     *", "   ",
+                        "*     *", "   ",
+                        "*      "),
+
+                String.join("", "*     *", "  ",
+                        "*     *", "   ",
+                        "****** ", "   ",
+                        " ***** "),
+
+                String.join("", "*     *", "  ",
+                        "*     *", "   ",
+                        "*      ", "   ",
+                        "      *"),
+
+                String.join("", "*     *", "  ",
+                        "*     *", "   ",
+                        "*      ", "   ",
+                        "      *"),
+
+                String.join("", " ***** ", "   ",
+                        " ***** ", "   ",
+                        "*      ", "   ",
+                        " ******")
+        };
+
+        // Enhanced for-loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
 }
