8edc7c589b58149d065ca8d47547a94c47783944
jaxcode23 <jashajmera008@gmail.com>
2026-03-02 08:32:17 +0530

    UC7: Implement CharacterPatternMap static inner class for scalable banner management

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,80 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****    *****    ******    ****** ");
-        System.out.println("*     *  *     *   *     *   *      ");
-        System.out.println("*     *  *     *   *     *   *      ");
-        System.out.println("*     *  *     *   ******    *****  ");
-        System.out.println("*     *  *     *   *              * ");
-        System.out.println("*     *  *     *   *              * ");
-        System.out.println(" *****    *****    *         ****** ");
+        String word = "OOPS";
+        String[] banner = new String[7];
 
+        for (int i = 0; i < 7; i++) {
+            StringBuilder lineBuilder = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+                CharacterPatternMap pattern = CharacterPatternMap.getPattern(ch);
+                lineBuilder.append(pattern.getPattern()[i]).append("   ");
+            }
+
+            banner[i] = lineBuilder.toString();
+        }
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+
+    // Static Inner Class for Character Pattern Management
+    static class CharacterPatternMap {
+
+        private char character;
+        private String[] pattern;
+
+        private static final Map<Character, CharacterPatternMap> patternMap = new HashMap<>();
+
+        static {
+            patternMap.put('O', new CharacterPatternMap('O', new String[]{
+                    " ***** ",
+                    "*     *",
+                    "*     *",
+                    "*     *",
+                    "*     *",
+                    "*     *",
+                    " ***** "
+            }));
+
+            patternMap.put('P', new CharacterPatternMap('P', new String[]{
+                    "****** ",
+                    "*     *",
+                    "*     *",
+                    "****** ",
+                    "*      ",
+                    "*      ",
+                    "*      "
+            }));
+
+            patternMap.put('S', new CharacterPatternMap('S', new String[]{
+                    " ******",
+                    "*      ",
+                    "*      ",
+                    " ***** ",
+                    "      *",
+                    "      *",
+                    " ******"
+            }));
+        }
+
+        public CharacterPatternMap(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
+
+        public static CharacterPatternMap getPattern(char ch) {
+            return patternMap.get(ch);
+        }
     }
 }
