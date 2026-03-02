a1c54fa635a64af8c6890678fe5beeead9bf41d1
jaxcode23 <jashajmera008@gmail.com>
2026-03-02 08:33:44 +0530

    UC8: Implement HashMap-based pattern storage and render via function

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,14 +1,72 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" *****    *****    ******    ****** ");
-        System.out.println("*     *  *     *   *     *   *      ");
-        System.out.println("*     *  *     *   *     *   *      ");
-        System.out.println("*     *  *     *   ******    *****  ");
-        System.out.println("*     *  *     *   *              * ");
-        System.out.println("*     *  *     *   *              * ");
-        System.out.println(" *****    *****    *         ****** ");
+        String word = "OOPS";
 
+        Map<Character, String[]> patternMap = buildPatternMap();
+
+        renderBanner(word, patternMap);
+    }
+
+    // Build pattern storage using HashMap
+    private static Map<Character, String[]> buildPatternMap() {
+
+        Map<Character, String[]> map = new HashMap<>();
+
+        map.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        map.put('P', new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        map.put('S', new String[]{
+                " ******",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ******"
+        });
+
+        return map;
+    }
+
+    // Render banner using nested loops + StringBuilder
+    private static void renderBanner(String word, Map<Character, String[]> patternMap) {
+
+        for (int row = 0; row < 7; row++) {
+
+            StringBuilder lineBuilder = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+
+                String[] pattern = patternMap.get(ch);
+
+                if (pattern != null) {
+                    lineBuilder.append(pattern[row]).append("   ");
+                }
+            }
+
+            System.out.println(lineBuilder);
+        }
     }
 }
